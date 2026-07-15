af433da16c47ba95ddf9dbbcfa29d1ab8b253d2b
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-07-15 16:59:25 +0300

    library-project
    Updated book card UI and book display functions with JS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const booksContainer = document.querySelector(".book-section");
 
 // bookId initially a parameter but removed cause it is generated automatically
 
@@ -18,3 +19,18 @@ function addBookToLibrary (title, author, rating, status){
     const book = new Book(title, author, rating, status);
     myLibrary.push(book);
 };
+
+addBookToLibrary("The color Purple", "Alice Walker", 5, "reading");
+addBookToLibrary("Those who perish", "George R. Martin", 5, "reading");
+
+console.log(myLibrary)
+
+function displayBook (array){
+    for (const book of array){
+        console.log(book.title, book.author, book.rating, book.status, book.bookId)
+    }
+};
+
+
+
+
