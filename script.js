261a5a12563512530d67a7557e530a24cf7eaa10
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-07-17 14:40:02 +0300

    library-project
    Added JavaScript logic to display library books

--- script.js
+++ script.js
@@ -21,15 +21,49 @@ function addBookToLibrary (title, author, rating, status){
 };
 
 addBookToLibrary("The color Purple", "Alice Walker", 5, "reading");
-addBookToLibrary("Those who perish", "George R. Martin", 5, "reading");
+
 
 console.log(myLibrary)
 
 function displayBook (array){
     for (const book of array){
-        console.log(book.title, book.author, book.rating, book.status, book.bookId)
-    }
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book");
+
+        const bookCover = document.createElement("div");
+        bookCover.classList.add("book-cover");
+
+        const bookMeta = document.createElement("div");
+        bookMeta.classList.add("book-meta")
+        
+
+        const bookTitle = document.createElement("p");
+        const bookAuthor = document.createElement("p");
+        const bookRating = document.createElement("p");
+        const bookStatus = document.createElement("p");
+        bookStatus.classList.add("book-status")
+        const bookID = document.createElement("p");
+
+        bookTitle.textContent = `${book.title}`;
+        bookAuthor.textContent = `By: ${book.author}`;
+        bookRating.textContent = `${book.rating} on Goodreads`;
+        bookStatus.textContent = `Status: ${book.status}`;
+        bookID.textContent = book.bookId;
+
+        booksContainer.appendChild(bookCard);
+        bookCard.appendChild(bookCover);
+        bookCard.appendChild(bookMeta);
+        bookMeta.appendChild(bookTitle);
+        bookMeta.appendChild(bookAuthor);
+        bookMeta.appendChild(bookRating);
+        bookMeta.appendChild(bookStatus);
+        bookMeta.appendChild(bookID);
+
+    };
 };
+
+
+displayBook(myLibrary)
 
 
 
