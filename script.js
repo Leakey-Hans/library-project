339ec334ed070e1dddb0a25bcb8420f09ae0537a
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-07-21 12:01:08 +0300

    library project
    complete library with book adding, display and removal js logic

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const myLibrary = [];
 const booksContainer = document.querySelector(".book-section");
+const addBookBtn = document.querySelector(".add-bookBtn");
+const bookForm = document.querySelector(".addBook-form");
+const formRemoveBtn = document.querySelector(".form-button");
+const addToLibraryBtn = document.querySelector(".form-button2");
+
+
+addBookBtn.addEventListener("click", () => {
+    bookForm.classList.remove("hidden");
+});
+
+formRemoveBtn.addEventListener("click", () => {
+    bookForm.classList.add("hidden");
+});
+
 
 // bookId initially a parameter but removed cause it is generated automatically
 
@@ -15,17 +29,18 @@ function Book(title, author, rating, status) {
     this.bookId = crypto.randomUUID();
 }
 
+//Adding Book to Array
+
 function addBookToLibrary (title, author, rating, status){
     const book = new Book(title, author, rating, status);
     myLibrary.push(book);
 };
 
-addBookToLibrary("The color Purple", "Alice Walker", 5, "reading");
 
-
-console.log(myLibrary)
+//Book Display Function
 
 function displayBook (array){
+    booksContainer.innerHTML = '';
     for (const book of array){
         const bookCard = document.createElement("div");
         bookCard.classList.add("book");
@@ -34,7 +49,20 @@ function displayBook (array){
         bookCover.classList.add("book-cover");
 
         const bookMeta = document.createElement("div");
-        bookMeta.classList.add("book-meta")
+        bookMeta.classList.add("book-meta");
+
+        const bookActions = document.createElement("div");
+        bookActions.classList.add("book-actions");
+
+        const deleteBtn = document.createElement("button");
+        const readBtn = document.createElement("button");
+        
+        deleteBtn.classList.add("book-actions-btn1")
+        readBtn.classList.add("book-actions-btn2")
+
+        deleteBtn.textContent = "Delete";
+        readBtn.textContent = "Mark as read"
+        
         
 
         const bookTitle = document.createElement("p");
@@ -53,18 +81,38 @@ function displayBook (array){
         booksContainer.appendChild(bookCard);
         bookCard.appendChild(bookCover);
         bookCard.appendChild(bookMeta);
+        bookCard.appendChild(bookActions);
+        bookActions.appendChild(deleteBtn);
+        bookActions.appendChild(readBtn);
         bookMeta.appendChild(bookTitle);
         bookMeta.appendChild(bookAuthor);
         bookMeta.appendChild(bookRating);
         bookMeta.appendChild(bookStatus);
         bookMeta.appendChild(bookID);
 
+
+        deleteBtn.addEventListener("click", () => {
+            booksContainer.removeChild(bookCard);
+            myLibrary.pop(book);
+        });
     };
+
 };
 
 
-displayBook(myLibrary)
+addToLibraryBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const titleValue = document.querySelector("#book-title").value;
+    const authorValue = document.querySelector("#author").value;
+    const ratingValue = document.querySelector("#rating").value;
+    const statusValue = document.querySelector("#status").value;
 
+    addBookToLibrary(titleValue, authorValue, ratingValue, statusValue);
+    displayBook(myLibrary);
+    bookForm.reset();
+    bookForm.classList.add("hidden");
+
+})
 
 
 
